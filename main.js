925d670cc1c8b8b598f49c83478f991ea85d1c7c
esmeebarr <esmeebarr@btinternet.com>
2021-05-25 14:26:53 +0100

    Cards styled

---
+++ main.js
@@ -0,0 +1,7 @@
+//WHAT MY GAME WILL NEED TO DO:
+//1. cards will be shuffled when game reloads
+//2. game will react correctly to matched or unmatched cards
+//3. game will show number f moves the player makes
+//4. a timer will start when game begins and stop when game ends
+//5. reset button will restart the game and timer
+//6. a winning message should appear when the game is completed showing time taken and a play again option
